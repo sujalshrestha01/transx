78043b16990a4540549b156b37fd8c2a3f34a642
Sujal Shrestha <sujal8696@gmail.com>
2026-05-26 10:06:49 +0545

    fix: move tailwind to devDependencies, add vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   server: {
-    allowedHosts: [
-      'interteam-runtishly-elane.ngrok-free.dev' // Add your ngrok host here
-    ],
+  server: {
     proxy: {
       '/api': {
-        // target: 'https://interteam-runtishly-elane.ngrok-free.dev',
         target: 'http://localhost:5000',
         changeOrigin: true,
       }
